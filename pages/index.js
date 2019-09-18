99577f5cd70cbc73fcd4335ed176c06e9c71af37
Julia Opatrny <jsopatrny@gmail.com>
2019-09-18 13:11:39 -0400

    hopefully this shit works

--- pages/index.js
+++ pages/index.js
@@ -2,90 +2,57 @@ import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import SimpleMenu from '../components/menu'
+import Grid from '@material-ui/core/Grid';
+import MediaCard from '../components/card';
+//import MonaLisa from '../static/monalisa.jpg;
 
 const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
-
-    <Nav />
-
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
+  // <Grid
+  // container
+  // direction="row"
+  // justify="center"
+  // alignItems="center"
+  // spacing={12}
+  // >
+  <Grid>
+    <div>
+      <SimpleMenu />
     </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
+    <Grid
+      container
+      direction="row"
+      justify="flex-start"
+      alignItems="center"
+    >
+        <MediaCard 
+        title="Mona Lisa" 
+        description="The Mona Lisa - 
+        by Leonardo Da Vinci. Mona Lisa, also known as La Gioconda, 
+        is the wife of Francesco del Giocondo. This painting is painted 
+        as oil on wood. The original painting size is 77 x 53 cm (30 x 20 7/8 in) 
+        and is owned by by the Government of France and is on the wall in the 
+        Louvre in Paris, France."
+        image="../static/monalisa.jpg"
+        />
+        <MediaCard 
+        title="Dance" 
+        description="Dance is a painting made by Henri Matisse in 1910, 
+        at the request of Russian businessman and art collector Sergei Shchukin, 
+        who bequeathed the large decorative panel to the Hermitage Museum in Saint 
+        Petersburg, Russia."
+        image="Dance"
+        />
+        <MediaCard 
+        title="Sunflowers" 
+        description="Sunflowers is the name of two series of still life paintings by 
+        the Dutch painter Vincent van Gogh. The first series, executed in Paris in 1887, 
+        depicts the flowers lying on the ground, while the second set, executed a year later in Arles, shows a bouquet of sunflowers in a vase."
+        image="Dance"
+        />
+        {/* <img src="/static/monalisa.jpg" alt="my image" /> */}
+    </Grid>
+  </Grid>
 )
 
 export default Home
