9844778998cc43c9a22488af8e05933d5e4f46a2
dholley1 <dholley@hamilton.edu>
2019-10-09 17:57:52 -0400

    more db stuff

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ const tileData = [
 
 
 
+
 class Home extends Component  {
 
   constructor(props){
@@ -66,8 +67,32 @@ class Home extends Component  {
   render() {
     return(
       <div>
-        <button onClick = {serverfuncs.postArtworkInfo}>post info</button>
-        <button onClick = {serverfuncs.getArtworkInfo}>get info</button>
+        <div>
+        <p></p>username:<input type = 'text' id = 'username'></input>
+        <p></p>password:<input type = 'text' id = 'password'></input>
+        <p></p>name:<input type = 'text' id = 'name'></input>
+        <p></p>admin:<input type = 'checkbox' id = 'admin'></input>
+        <p></p>guilders:<input type = 'number' id = 'guilders'></input>
+        <p></p>micro:<input type = 'number' id = 'micropoints'></input>
+        <p></p>paintings:<input type = 'text' id = 'paintings'></input>
+        </div>
+        <button onClick = {serverfuncs.createUser}>CREATE ACCOUNT</button>
+        <button onClick = {serverfuncs.logInUser}>LOG IN</button>
+        <button onClick = {serverfuncs.logOutUser}>LOG OUT</button>
+        <p id = 'mytext'>NULL</p>
+        <div>
+          <button onClick = {serverfuncs.createArtPost}>CREATE ART POST</button>
+        </div>
+        <div>
+          <p></p>title:<input type = 'text' id = 'title'></input>
+          <p></p>artist:<input type = 'text' id = 'artist'></input>
+          <p></p>year:<input type = 'text' id = 'year'></input>
+          <p></p>theoretical:<input type = 'number' id = 'theoretical'></input>
+          <p></p>actual:<input type = 'number' id = 'actual'></input>
+          <p></p>hidden:<input type = 'checkbox' id = 'hidden'></input>
+          <p></p>owner:<input type = 'text' id = 'owner'></input>
+          <p></p>url:<input type = 'text' id = 'url'></input>
+        </div>
         <AppBar />
         {/* <PinGrid /> */}
         {/* <GridList /> */}
--- src/serverfuncs.js
+++ src/serverfuncs.js
@@ -1,45 +1,193 @@
-export { getArtworkInfo, postArtworkInfo };
+/* eslint-disable require-jsdoc */
+export {getArtworkInfo, putArtworkInfo, deleteArtworkInfo,
+  logInUser, logBackInUser, logOutUser, createUser, createArtPost, checkForTrade};
 
-var artident = "monalisaleo";
-var studentident = "dholley";
-
-// function getArtworkInfo() {
-//     fetch('http://localhost:9000/artworks/monalisaleo/', {
-//         method: 'get',
-//     }).then(function (res) {
-//         console.log(res);
-//         const artwork = res.json;
-//         console.log(artwork);
-//         console.log("title: "+artwork["title"]);
-//         console.log("artist: "+artwork["artist"]);
-//         console.log("year: "+artwork["year"]);
-//         console.log("theoreticalprice: "+artwork["theoreticalprice"]);
-//         console.log("actualprice: "+artwork["actualprice"]);
-//         console.log("hidden: "+artwork["hidden"]);
-//         console.log("owner: "+artwork["owner"]);
-//         console.log("url: "+artwork["url"]);
-//     })
-// }
-
-async function getArtworkInfo() {
-    const response = await fetch('http://localhost:9000/artworks/monalisaleo/');
-    const myJson = await response.json();
-    console.log(JSON.stringify(myJson));
-    var artwork = JSON.parse(JSON.stringify(myJson))['0'];
-    console.log(artwork);
-    console.log("title: "+artwork["title"]);
-    console.log("artist: "+artwork["artist"]);
-    console.log("year: "+artwork["year"]);
-    console.log("theoreticalprice: "+artwork["theoreticalprice"]);
-    console.log("actualprice: "+artwork["actualprice"]);
-    console.log("hidden: "+artwork["hidden"]);
-    console.log("owner: "+artwork["owner"]);
-    console.log("url: "+artwork["url"]);
+if (localStorage.getItem('username') === 'dholley') {
+  logBackInUser();
 }
 
-function postArtworkInfo () {
-    fetch('http://localhost:9000/artworks/', {
-        method: 'post',
+function coroutine(f) {
+  const o = f();
+  o.next();
+  return function(x) {
+    o.next(x);
+  };
+}
+let tradeRequest = false;
+const tradeCheck = coroutine(function* () {
+  while (true) {
+    yield;
+    checkForTrade();
+    if (tradeRequest) {
+      console.log('trade requested');
+    }
+  }
+});
+
+//setInterval(tradeCheck, 1000);
+
+async function checkForTrade() {
+  const response = await fetch('http://localhost:9000/students/dholley');
+  const myJson = await response.json();
+  const student = JSON.parse(JSON.stringify(myJson))['0'];
+  if (typeof student !== 'undefined') {
+    tradeRequest = true;
+  } else {
+    console.log('no trade');
+  }
+}
+
+/*
+
+
+            USER MANAGEMENT FUNCS
+
+
+*/
+
+async function logInUser() {
+  const stringName = document.getElementById('username').value;
+  const response = await fetch('http://localhost:9000/students/'+stringName);
+  const myJson = await response.json();
+  const student = JSON.parse(JSON.stringify(myJson))['0'];
+  const output = document.getElementById('mytext');
+  if (typeof student !== 'undefined') {
+    output.innerHTML = 'success!';
+    localStorage.setItem('username', document.getElementById('username').value);
+    console.log(student);
+  } else {
+    output.innerHTML = 'does not exist :(';
+    console.log('entry does not exits!');
+  }
+}
+
+async function logBackInUser() {
+  const stringName = localStorage.getItem('username');
+  const response = await fetch('http://localhost:9000/students/'+stringName);
+  const myJson = await response.json();
+  const student = JSON.parse(JSON.stringify(myJson))['0'];
+  const output = document.getElementById('mytext');
+  if (typeof student !== 'undefined') {
+    output.innerHTML = 'success!';
+    console.log(student);
+  } else {
+    output.innerHTML = 'does not exist :(';
+    console.log('entry does not exits!');
+  }
+}
+
+function logOutUser() {
+  localStorage.clear();
+  window.location.reload();
+}
+
+/*
+
+            ADMIN STUDENT MANAGEMENT FUNCS
+
+*/
+
+
+function createUser() {
+  fetch('http://localhost:9000/students/', {
+    method: 'post',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+        {username: document.getElementById('username').value,
+          hash: document.getElementById('password').value,
+          name: document.getElementById('name').value,
+          admin: document.getElementById('admin').checked,
+          guilders: parseInt(document.getElementById('guilders').value),
+          microresearchpoints: parseInt(
+              document.getElementById('micropoints').value),
+          paintings: document.getElementById('paintings').value,
+        }),
+  }).then(function(res) {
+    console.log(res);
+  })
+}
+
+
+function deleteUser() {
+  fetch('http://localhost:9000/students/dholley', {
+    method: 'delete',
+    mode: 'cors',
+  }).then(function(res) {
+    console.log(res);
+  })
+}
+
+/*
+
+            ADMIN ARTWORK MANAGEMENT FUNCS
+
+*/
+
+function createArtPost() {
+  fetch('http://localhost:9000/artworks/', {
+    method: 'post',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+        {title: document.getElementById('title').value,
+          artist: document.getElementById('artist').value,
+          year: document.getElementById('year').value,
+          theoreticalprice: parseInt(
+              document.getElementById('theoretical').value),
+          actualprice: parseInt(document.getElementById('actual').value),
+          hidden: document.getElementById('hidden').checked,
+          owner: document.getElementById('owner').value,
+          url: document.getElementById('url').value,
+        }),
+  }).then(function(res) {
+    console.log(res);
+  })
+}
+
+function deleteArtworkInfo() {
+  fetch('http://localhost:9000/artworks/monalisa', {
+    method: 'delete',
+    mode: 'cors',
+  }).then(function(res) {
+    console.log(res);
+  })
+}
+
+/*
+
+            ARTWORK MANAGEMENT FUNCS
+
+*/
+
+async function getArtworkInfo() {
+  const response = await fetch('http://localhost:9000/artworks/'+DocumentFragment.getElementById('artwork').value);
+  const myJson = await response.json();
+  console.log(JSON.stringify(myJson));
+  const artwork = JSON.parse(JSON.stringify(myJson))['0'];
+  if (typeof artwork !== 'undefined') {
+    console.log(artwork);
+    console.log('title: '+artwork['title']);
+    console.log('artist: '+artwork['artist']);
+    console.log('year: '+artwork['year']);
+    console.log('theoreticalprice: '+artwork['theoreticalprice']);
+    console.log('actualprice: '+artwork['actualprice']);
+    console.log('hidden: '+artwork['hidden']);
+    console.log('owner: '+artwork['owner']);
+    console.log('url: '+artwork['url']);
+  }
+  else {
+    console.log('artwork does not exist');
+  }
+}
+
+function putArtworkInfo() {
+    fetch('http://localhost:9000/artworks/monalisa', {
+        method: 'put',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
@@ -49,7 +197,7 @@ function postArtworkInfo () {
                 artist: 'leo',
                 year: 1500,
                 theoreticalprice: 100,
-                actualprice: 200,
+                actualprice: 150,
                 hidden: true,
                 owner: 'dholley',
                 url: "none"   
@@ -59,10 +207,41 @@ function postArtworkInfo () {
     })
 }
 
-function getStudentInfo() {
-    fetch('http://localhost:9000/artworks/'+studentident, {
-        method: 'get'
-    }).then(function (res) {
-        const jsonfile = res.json();
-    })
+async function makeTrade() {
+  const response = await fetch('http://localhost:9000/students/dholley');
+  const myJson = await response.json();
+  const student = JSON.parse(JSON.stringify(myJson))['0'];
+
+  student.guilders -= 0; //value entered to give 
+  student.guilders += 0; //value entered to recieve
+
+  const artworks = ["monalisa", "starrynight"] ;
+
+  for (const artwork in artworks) {
+      fetch('http://localhost:9000/owners/'+artwork, {
+        method: 'put',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          name: 'monalisa',
+          owner: 'dholley'
+        },
+      }).then(function(res) {
+        console.log(res);
+      })
+  }
+  fetch('http://localhost:9000/artworks/dholley', {
+    method: 'put',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: student,
+  }).then(function(res) {
+    console.log(res);
+  })
 }
+
+
