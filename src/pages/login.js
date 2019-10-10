837ffc5b37408f8cb59282e622921ccb99e28a4f
Julia Opatrny <jsopatrny@gmail.com>
2019-10-09 22:01:22 -0400

    fixed errors

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import Login from "../components/login";
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+
+const Login = () => {
+        return(
+            <div>
+                <h1>Login</h1>
+                <p>title:<input type = 'text' id = 'title'></input></p>
+            </div>
+        )
+    
+}
+
+export default Login
