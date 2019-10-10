98e58e0d8a49f1ac4b106d041ec61ace06039565
Julia Opatrny <jsopatrny@gmail.com>
2019-10-09 20:43:21 -0400

    multipage

---
+++ src/pages/error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+        return(
+            <div>
+                <h2>Error 404 Page Not Found</h2>
+            </div>
+        )
+    
+}
+
+export default ErrorPage
---
+++ src/pages/table.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "../components/table";
+
+const TablePage = () => {
+        return(
+            <div>
+                <Table />
+            </div>
+        )
+    
+}
+
+export default TablePage
