1c3f4f3a98e629bb067a14d568797ebd637f5018
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-10-11 15:33:11 -0400

    modified api to better reflect our server's parameters

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,19 +6,19 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'fantasy_collecting',
+  user: 'fantasyc_user',
+  password: 'o6UZMvPZas0H',
+  database: 'fantasyc_database',
 });
 
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM students', (err, results, fields) => {
+  connection.query('SELECT * FROM users', (err, results, fields) => {
     res.send(results);
   });
 });
 
 router.get('/:id', function(req, res, next) {
-  connection.query(`SELECT * FROM students WHERE username = '${req.params.id}'`, (err, results, fields) => {
+  connection.query(`SELECT * FROM users WHERE username = '${req.params.id}'`, (err, results, fields) => {
     res.send(results);
   });
 });
@@ -35,7 +35,7 @@ router.post('/', json(), function(req, res, next) {
       req.body.admin,
       req.body.guilders,
       req.body.microresearchpoints,
-      req.body.paintings,
+      req.body.numofpaintings,
     ];
 
     for (const i in dbEntry) {
@@ -48,7 +48,7 @@ router.post('/', json(), function(req, res, next) {
 
     const dbEntryArgs = dbEntry.join(', ');
 
-    connection.query(`INSERT INTO students VALUES (${dbEntryArgs})`, (err, results, fields) => {
+    connection.query(`INSERT INTO users VALUES (${dbEntryArgs})`, (err, results, fields) => {
       if (err) {
         console.error(err);
         res.sendStatus(500);
@@ -66,15 +66,15 @@ router.put('/:id', json(), function(req, res, next) {
     'admin': req.body.admin,
     'guilders': req.body.guilders,
     'microresearchpoints': req.body.microresearchpoints,
-    'paintings': req.body.paintings,
+    'numofpaintings': req.body.numofpaintings,
   };
 
   for (const item of Object.keys(dbEntry)) {
     if (dbEntry[item] != undefined) {
       if (typeof(dbEntry[item]) == 'string') {
-        connection.query(`UPDATE students SET ${item} = '${dbEntry[item]}' WHERE username = '${req.params.id}'`);
+        connection.query(`UPDATE users SET ${item} = '${dbEntry[item]}' WHERE username = '${req.params.id}'`);
       } else {
-        connection.query(`UPDATE students SET ${item} = ${dbEntry[item]} WHERE username = '${req.params.id}'`);
+        connection.query(`UPDATE users SET ${item} = ${dbEntry[item]} WHERE username = '${req.params.id}'`);
       }
     }
   }
@@ -83,7 +83,7 @@ router.put('/:id', json(), function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  connection.query(`DELETE FROM students WHERE username = '${req.params.id}'`, (err, results, fields) => {
+  connection.query(`DELETE FROM users WHERE username = '${req.params.id}'`, (err, results, fields) => {
     res.sendStatus(200);
   });
 });
