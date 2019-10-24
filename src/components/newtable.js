483b7267c2c168e99c392801d0bdc3183394c965
Julia Opatrny <jsopatrny@gmail.com>
2019-10-24 12:41:12 -0400

    add row work

---
+++ src/components/newtable.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+//import EditIcon from 'material-ui/svg-icons/image/edit';
+//import Delete from 'material-ui/svg-icons/action/delete';
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+        { title: 'Username', field: 'username' },
+      { title: 'Name', field: 'name' },
+      { title: 'Money', field: 'money' },
+      { title: 'Paintings', field: 'paintings', type: 'numeric' },
+      { title: 'Value', field: 'value', type: 'numeric' },
+      { title: 'Kudos', field: 'kudos' },
+      {
+        title: 'Birth Place',
+        field: 'birthCity',
+        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+      },
+    ],
+    data: [
+      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+      {
+        name: 'Zerya Betül',
+        surname: 'Baran',
+        birthYear: 2017,
+        birthCity: 34,
+      },
+    ],
+  });
+
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
