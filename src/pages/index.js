22b0408299b96728a6a7dc132088b521d864fb26
Julia Opatrny <jsopatrny@gmail.com>
2019-10-27 18:35:39 -0400

    appbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,57 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Homepage from "../components/homepage";
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { Link } from "react-router-dom";
+
+function SimpleMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleClick = event => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    return (
+      <div>
+          <IconButton edge="start" color="inherit" aria-label="menu"
+          aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                <MenuIcon />
+            </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}><Link to="/table" style={{color: "#000000", textDecoration: "none"}}>Table</Link></MenuItem>
+          <MenuItem onClick={handleClose}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log In</Link></MenuItem>
+        </Menu>
+      </div>
+    );
+  }
 
 const MainPage = () => {
         return(
             <div>
-                <Link to="/table">Table</Link><p></p>
-                <Link to="/login">Log In</Link>
+                <AppBar position="static">
+                    <Toolbar variant="dense">
+                        {SimpleMenu()}
+                        <Typography variant="h6" color="inherit">
+                            Fantasy Collecting
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
                 <Homepage />
             </div>
         )
