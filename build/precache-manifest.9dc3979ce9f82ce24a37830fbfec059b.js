1e5518d77c9d02fba0193406abc6f6cec0dc032e
dholley1 <dholley@hamilton.edu>
2019-10-27 19:36:53 -0400

    gitignore

---
+++ build/precache-manifest.9dc3979ce9f82ce24a37830fbfec059b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c569df55baae06c08a74adebb4be717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c9e1856a61acfda53d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aceb710a864f0414eb1a",
+    "url": "/static/js/2.9c6e90fa.chunk.js"
+  },
+  {
+    "revision": "56c9e1856a61acfda53d",
+    "url": "/static/js/main.1ebcedbb.chunk.js"
+  },
+  {
+    "revision": "2704b45f9821d4247436",
+    "url": "/static/js/runtime-main.238ac574.js"
+  },
+  {
+    "revision": "515325b98a89074f45c1198ec2402716",
+    "url": "/static/media/coin.515325b9.png"
+  },
+  {
+    "revision": "94d13e1979801cdeb4d0cc7a103eb5eb",
+    "url": "/static/media/dance.94d13e19.jpg"
+  },
+  {
+    "revision": "d7a593e909e0392067a83cde7053ac37",
+    "url": "/static/media/menuIcon.d7a593e9.png"
+  },
+  {
+    "revision": "c08cd1513739cdb9e4ce452b6ba9e51c",
+    "url": "/static/media/monalisa.c08cd151.jpg"
+  },
+  {
+    "revision": "7ebd97b85704eb464ca52cb8116bce8f",
+    "url": "/static/media/sunflowers.7ebd97b8.jpg"
+  }
+]);
