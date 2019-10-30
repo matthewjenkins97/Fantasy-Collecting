955d44bd5e2a6355b8df6b23da5f3ee32c2c4278
Julia Opatrny <jsopatrny@gmail.com>
2019-10-30 19:17:03 -0400

    login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,43 +1,78 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+//import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import * as serverfuncs from '../serverfuncs';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// @media all and (min-width: 480px) {
+//   .Login {
+//     padding: 60px 0;
+//   }
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+//   .Login form {
+//     margin: 0 auto;
+//     max-width: 320px;
+//   }
+// }
+
+class Login extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { values: {username: '', password: ''}};
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange(event) {
+    this.setState({username: event.target.username});
+    this.setState({password: event.target.password});
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
+  render(){
+    return (
+      <div className="Login">
+                <h1 style={{textAlign: "center"}}>Login</h1>
+                <div style={{alignItems: "center", textAlign: "center"}}>
+                  {/* <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Essay:
+                      <textarea value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                  </form> */}
+                  <div>
+                    <TextField
+                      id="liusername"
+                      label="Username"
+                      margin="normal"
+                      variant="outlined"
+                      value={this.state.username}
+                      onChange={(this.handleChange)}
+                    />
+                  </div>
+                  <div>
+                    <TextField
+                      id="lipassword"
+                      label="Password"
+                      margin="normal"
+                      variant="outlined"
+                      type="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <Button variant="contained" 
+                  color="primary"
+                  style={{marginTop: 10}}
+                  onClick={serverfuncs.logInUser}>Log In
+                  </Button>
+                  {/* username:<input type = 'text' id = 'liusername'></input>
+                  <p></p>password:<input type = 'text' id = 'lipassword'></input>
+                  <p></p><button onClick = {serverfuncs.logInUser}>log in</button>
+                  <p></p><button onClick = {serverfuncs.logOutUser}>log out</button> */}
+                </div>
+      </div>
+    );
+   }
 }
+
+export default Login
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import Login from "../components/login";
+import Login from "../components/login";
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 import * as serverfuncs from "../serverfuncs";
@@ -9,14 +9,15 @@ console.log(window.location);
 if (localStorage.getItem('username') !== null && window.location.pathname == '/login') {
     serverfuncs.logBackInUser();
 }
-const Login = () => {
+const LoginPage = () => {
         return(
             <div>
-                <h1>Login</h1>
+                <Login />
+                {/* <h1>Login</h1>
                 username:<input type = 'text' id = 'liusername'></input>
                 <p></p>password:<input type = 'text' id = 'lipassword'></input>
                 <p></p><button onClick = {serverfuncs.logInUser}>log in</button>
-                <p></p><button onClick = {serverfuncs.logOutUser}>log out</button>
+                <p></p><button onClick = {serverfuncs.logOutUser}>log out</button> */}
             </div>
         )
     
