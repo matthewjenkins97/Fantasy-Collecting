b28e8efede28f7fd116cb91916dad3fd7a62c5ae
dholley1 <dholley@hamilton.edu>
2019-10-30 20:15:00 -0400

    new table bugs

--- src/components/newtable.js
+++ src/components/newtable.js
@@ -7,26 +7,7 @@ import * as serverfuncs from '../serverfuncs';
 var rows = [];
 var read = false;
 
-async function getRows() {
-  rows = [];
-  const users = await serverfuncs.getAllUsers();
-  for(var user in users) {
-    console.log(user);
-    var dict = {username: user.username, 
-      name: user.name, 
-      money: user.guilders, 
-      paintings: user.numofpaintings,  
-      value: 0,
-      kudos: user.microresearchpoints};
-    rows.push(dict);
-  };
-  this.forceUpdate()
-}
-
-
-export default function MaterialTableDemo() {
-  const [state, setState] = React.useState({
-    columns: [
+var stateBeg = {columns: [
         { title: 'Username', field: 'username' },
       { title: 'Name', field: 'name' },
       { title: 'Money', field: 'money' },
@@ -34,49 +15,94 @@ export default function MaterialTableDemo() {
       { title: 'Value', field: 'value', type: 'numeric' },
       { title: 'Kudos', field: 'kudos' },
     ],
-    data: rows,//[
-    //   { username: 'jopatrny', name: 'Julia Opatrny', money: 9000, paintings: 5, value: 200, kudos: 50 },
-    //   { username: 'dholley', name: 'Donald Holley', money: 4000, paintings: 6, value: 400, kudos: 35 },
-    //   { username: 'mjenkins', name: 'Matt Jenkins', money: 1500, paintings: 8, value: 350, kudos: 85 },
-    // ],
-  });
+    data: rows,
+}
 
-  return (
-    <MaterialTable
-      title="Users"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.push(newData);
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data[data.indexOf(oldData)] = newData;
-              setState({ ...state, data });
-              serverfuncs.updateUserData(newData);
-              console.log(oldData);
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.splice(data.indexOf(oldData), 1);
-              setState({ ...state, data });
-            }, 600);
-          }),
-      }}
-    />
-  );
+export default class MaterialTableDemo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getRows();
+    this.state = stateBeg; 
+  }
+  async getRows() {
+    rows = [];
+    const users = await serverfuncs.getAllUsers();
+    for(var user of users) {
+      console.log(user);
+      var dict = {username: user.username, 
+        name: user.name, 
+        money: user.guilders, 
+        artworks: user.numofpaintings,
+        value: 0,
+        kudos: user.microresearchpoints};
+      rows.push(dict);
+    };
+    this.state.data = rows;
+    this.state.data = this.state.data.sort(function(a, b){return a.username[0] > b.username[0] ? 1 : -1});
+    read = true;
+    this.forceUpdate();
+  }
+  render() {
+    // const [state, setState] = React.useState({
+    //   columns: [
+    //       { title: 'Username', field: 'username' },
+    //     { title: 'Name', field: 'name' },
+    //     { title: 'Money', field: 'money' },
+    //     { title: 'Artworks', field: 'artworks', type: 'numeric' },
+    //     { title: 'Value', field: 'value', type: 'numeric' },
+    //     { title: 'Kudos', field: 'kudos' },
+    //   ],
+    //   data: rows,//[
+    //   //   { username: 'jopatrny', name: 'Julia Opatrny', money: 9000, paintings: 5, value: 200, kudos: 50 },
+    //   //   { username: 'dholley', name: 'Donald Holley', money: 4000, paintings: 6, value: 400, kudos: 35 },
+    //   //   { username: 'mjenkins', name: 'Matt Jenkins', money: 1500, paintings: 8, value: 350, kudos: 85 },
+    //   // ],
+    // });
+    return (
+      <div>
+      {read ? (
+      <MaterialTable
+        title="Users"
+        columns={this.state.columns}
+        data={this.state.data}
+        editable={{
+          onRowAdd: newData =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                const data = [rows];
+                data.push(newData);
+                rows = data;
+                //setState({ ...state, data });
+              }, 600);
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise(resolve => {
+              //this.data[this.data.indexOf(oldData)] = newData;
+              setTimeout(() => {
+                resolve();
+                //var data = [...this.state.data];
+                this.state.data = this.state.data.filter(function(value, index, arr){
+                  return arr[index].username !== oldData.username;
+                });
+                this.state.data.push(newData);
+                this.state.data = this.state.data.sort(function(a, b){return a.username[0] > b.username[0] ? 1 : -1});
+                this.setState({ ...this.state, ...this.state.data });
+                serverfuncs.updateUserData(newData);
+              }, 600);
+            }),
+          onRowDelete: oldData =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                const data = [rows];
+                this.state.splice(data.indexOf(oldData), 1);
+                
+                //setState({ ...state, data });
+              }, 600);
+            }),
+        }}
+      />
+    ) : (<h1>Loading...</h1>)} </div> ); 
+  }
 }
