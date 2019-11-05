d6c70169ee57783b5d4a87bc26a0558ab53e066d
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-11-04 20:31:07 -0500

    started trades api

--- api/routes/trades.js
+++ api/routes/trades.js
@@ -12,31 +12,34 @@ const connection = mysql.createPool({
 });
 
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM users', (err, results, fields) => {
+  connection.query('SELECT * FROM trades', (err, results, fields) => {
     res.send(results);
   });
 });
 
 router.get('/:id', function(req, res, next) {
-  connection.query(`SELECT * FROM users WHERE username = '${req.params.id}'`, (err, results, fields) => {
+  connection.query(`SELECT * FROM trades WHERE seller = '${req.params.id}'`, (err, results, fields) => {
     res.send(results);
   });
 });
 
 router.post('/', json(), function(req, res, next) {
   // primary key check - if it doesn't exist, it's a bad request
-  if (!req.body.username) {
+  if (!req.body.seller) {
     res.sendStatus(400);
   } else {
     const dbEntry = [
-      req.body.username,
-      req.body.hash,
-      req.body.name,
-      req.body.admin,
-      req.body.guilders,
-      req.body.microresearchpoints,
-      req.body.numofpaintings,
+      req.body.buyer,
+      req.body.seller,
+      req.body.buyerinit,
+      req.body.sellerinit,
+      req.body.buyerapproved,
+      req.body.sellerapproved,
+      req.body.timestamp,
     ];
+
+    dbEntry[6] = new Date(dbEntry[4]).toISOString().slice(0, 19).replace('T', ' ');
+
 
     for (const i in dbEntry) {
       if (typeof(dbEntry[i]) === 'string') {
@@ -48,7 +51,7 @@ router.post('/', json(), function(req, res, next) {
 
     const dbEntryArgs = dbEntry.join(', ');
 
-    connection.query(`INSERT INTO users VALUES (${dbEntryArgs})`, (err, results, fields) => {
+    connection.query(`INSERT INTO trades VALUES (${dbEntryArgs})`, (err, results, fields) => {
       if (err) {
         console.error(err);
         res.sendStatus(500);
@@ -61,20 +64,15 @@ router.post('/', json(), function(req, res, next) {
 
 router.put('/:id', json(), function(req, res, next) {
   const dbEntry = {
-    'hash': req.body.hash,
-    'name': req.body.name,
-    'admin': req.body.admin,
-    'guilders': req.body.guilders,
-    'microresearchpoints': req.body.microresearchpoints,
-    'numofpaintings': req.body.numofpaintings,
+
   };
 
   for (const item of Object.keys(dbEntry)) {
     if (dbEntry[item] != undefined) {
       if (typeof(dbEntry[item]) == 'string') {
-        connection.query(`UPDATE users SET ${item} = '${dbEntry[item]}' WHERE username = '${req.params.id}'`);
+        connection.query(`UPDATE trades SET ${item} = '${dbEntry[item]}' WHERE seller = '${req.params.id}'`);
       } else {
-        connection.query(`UPDATE users SET ${item} = ${dbEntry[item]} WHERE username = '${req.params.id}'`);
+        connection.query(`UPDATE trades SET ${item} = ${dbEntry[item]} WHERE seller = '${req.params.id}'`);
       }
     }
   }
@@ -83,7 +81,7 @@ router.put('/:id', json(), function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  connection.query(`DELETE FROM users WHERE username = '${req.params.id}'`, (err, results, fields) => {
+  connection.query(`DELETE FROM trades WHERE seller = '${req.params.id}'`, (err, results, fields) => {
     res.sendStatus(200);
   });
 });
