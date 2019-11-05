4b94e5c573c26477e8d6cd730ae7534d9da407c8
Julia Opatrny <jsopatrny@gmail.com>
2019-11-04 19:22:10 -0500

    remove log in

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ function SimpleMenu() {
           onClose={handleClose}
         >
           <MenuItem onClick={handleClose}><Link to="/table" style={{color: "#000000", textDecoration: "none"}}>Table</Link></MenuItem>
-          <MenuItem onClick={handleClose}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log In</Link></MenuItem>
+          {/* <MenuItem onClick={handleClose}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log In</Link></MenuItem> */}
           <MenuItem onClick={() => (serverfuncs.logOutUser())}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log Out</Link></MenuItem>
         </Menu>
       </div>
--- src/pages/table.js
+++ src/pages/table.js
@@ -35,7 +35,7 @@ function SimpleMenu() {
           onClose={handleClose}
         >
           <MenuItem onClick={handleClose}><Link to="/" style={{color: "#000000", textDecoration: "none"}}>My Gallery</Link></MenuItem>
-          <MenuItem onClick={handleClose}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log In</Link></MenuItem>
+          {/* <MenuItem onClick={handleClose}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log In</Link></MenuItem> */}
           <MenuItem onClick={() => (serverfuncs.logOutUser())}><Link to="/login" style={{color: "#000000", textDecoration: "none"}}>Log Out</Link></MenuItem>
         </Menu>
       </div>
