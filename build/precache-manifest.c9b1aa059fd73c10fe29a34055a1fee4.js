624208b9220766382cbe9bee3232b9e6dadb2a1c
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-12-16 11:17:56 -0500

    added build

---
+++ build/precache-manifest.c9b1aa059fd73c10fe29a34055a1fee4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624950ef7f1e5cad549797e4a9fcabe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6f80426da433da595b",
+    "url": "/static/css/main.8b099e48.chunk.css"
+  },
+  {
+    "revision": "cf8ad7be7d1185f5e429",
+    "url": "/static/js/2.70598cb4.chunk.js"
+  },
+  {
+    "revision": "3a6f80426da433da595b",
+    "url": "/static/js/main.f2488316.chunk.js"
+  },
+  {
+    "revision": "f4f9fe7bd2d6be3f2a27",
+    "url": "/static/js/runtime-main.40627eef.js"
+  },
+  {
+    "revision": "697b9f8bf8e6141d475053b6d8af32a8",
+    "url": "/static/media/auctionhouse.697b9f8b.jpg"
+  },
+  {
+    "revision": "c82accdd0273a9d96222841ccdbd7547",
+    "url": "/static/media/background.c82accdd.jpg"
+  },
+  {
+    "revision": "515325b98a89074f45c1198ec2402716",
+    "url": "/static/media/coin.515325b9.png"
+  },
+  {
+    "revision": "9110e9e5156612568ba927487da7e906",
+    "url": "/static/media/guilder.9110e9e5.svg"
+  },
+  {
+    "revision": "d7a593e909e0392067a83cde7053ac37",
+    "url": "/static/media/menuIcon.d7a593e9.png"
+  }
+]);
