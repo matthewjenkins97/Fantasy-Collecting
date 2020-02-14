9885a7081a296c863f01cc2b77e294b94116545e
donaldholley <dholleydev@gmail.com>
2020-02-14 14:32:21 -0500

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cc94ea4068c32c4149bac601a96c25d.js"
+  "/precache-manifest.4651741c4b2934e181bc5dca64f32250.js"
 );
 
 self.addEventListener('message', (event) => {
